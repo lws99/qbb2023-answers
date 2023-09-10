05d4af57c4b9585bc7895dcc7dfa61439c87c7d5
Logan Wallace Shepard <lshepar8@jh.edu>
2023-09-10 17:00:47 -0400

    Add final answers for all exercises

--- day4-homework/Wright_Fisher_Model_Assignment.py
+++ day4-homework/Wright_Fisher_Model_Assignment.py
@@ -34,7 +34,15 @@ import matplotlib.pyplot as plt
 #np.random.binomial(n,p)
 
 
-#Creating a function that will return allele frequencies for each generation of a population of at least 100
+
+
+
+
+
+
+#EXERCISE 1
+
+#PART1: Creating a function that will return allele frequencies for each generation of a population of at least 100
 def W_F_model(starting_allele_freq, pop_size=100):
     #find the allele freq of the next pop
     allele_freq=[]
@@ -44,25 +52,22 @@ def W_F_model(starting_allele_freq, pop_size=100):
         allele_freq.append(starting_allele_freq)
     return allele_freq
 
-
 #finding the allele frequencies for an allele with  starting frequency of 0.8 in a population of 100 using W_F_model
 allele_frequencies_test=W_F_model(0.5)
 #print(allele_frequencies_test)
 
-#finding the number of generations it will take for the allele to reach fixation
+
+
+#PART2: finding the number of generations it will take for the allele to reach fixation
 print(len(allele_frequencies_test))
 
 
 
-
-# Plot data
-
+#PART3: Plotting allele trajectory --> plt.show() is at the end of my code, so when you run the entire thing it will show all of the graphs
 
 #preparing the variables for plotting
 generation=range(len(allele_frequencies_test))
 frequency=allele_frequencies_test
-
-
 
 #plotting time to allele fixation for one model iteration
 fig0, ax = plt.subplots()
@@ -77,9 +82,13 @@ ax.plot(generation, frequency, c = "red")
 
 
 
+
+
+
+
 #EXERCISE 2
 
-#plotting allele frequency trajectories across generations for 30 model iterations
+#PART1: plotting allele frequency trajectories across generations for 30 model iterations --> plt.show() is at the end of my code, so when you run the entire thing it will show all of the graphs
 fig1, ax=plt.subplots()
 for i in range(30):
     allele_frequencies_test=W_F_model(0.5)
@@ -93,7 +102,7 @@ for i in range(30):
 
 
 
-#making a histogram of times to fixation
+#PART2: making a histogram of times to fixation --> plt.show() is at the end of my code, so when you run the entire thing it will show all of the graphs
 fig2, ax=plt.subplots()
 for i in range(1000):
     allele_frequencies_test=W_F_model(0.5)
@@ -118,8 +127,6 @@ ax.hist(frequency)
 #Pick at least five population sizes greater than or equal to 50. For each population size, run the model at least 50 times and find the average time to fixation. 
 #Keep your allele frequency constant for all runs. Create a scatter or line plot of population size vs. average time to fixation.
 
-#affect of population size on allele fixation
-
 
 #create a list of different population sizes
 size_of_population=[50,60,70,80,90]
@@ -138,8 +145,7 @@ for p in size_of_population:
 print(mean_final)
 
 
-
-#plotting the data size vs average time to fixation
+#PART1: plotting the population size vs average time to fixation -->plt.show() is at the end of my code, so when you run the entire thing it will show all of the graphs
 fig3, ax=plt.subplots()
 
 population=size_of_population
@@ -151,9 +157,10 @@ ax.scatter(population, time_fixation)
 
 
 
+
+
 #This time, pick a population size and vary the allele frequency. Run at least 10 trials for each allele frequency.
 #affect of allele frequency on fixation
-
 
 #create a list of different frequencies
 diff_starting_freqs=[0.2,0.4,0.6,0.8]
@@ -170,7 +177,7 @@ print(mean_final_freqs_fixation)
 
 
 
-#plotting the starting frequencies vs mean time to fixation
+#PART2: plotting the starting frequencies vs mean time to fixation -->plt.show() is at the end of my code, so when you run the entire thing it will show all of the graphs
 fig4, ax=plt.subplots()
 
 changing_starting_freqs=diff_starting_freqs
@@ -181,6 +188,9 @@ ax.set_ylabel("mean time to fixation")
 ax.scatter(changing_starting_freqs, time_to_fixation)
 
 
+
+
+
 plt.show()
 
 
@@ -189,5 +199,41 @@ plt.close( fig1 )
 plt.close( fig2 )
 plt.close(fig3)
 plt.close(fig4)
+
+
+
+
+
+#BASIC EXERCISE: INTERPRETING DATA
+#For any plot, explain the results you see. What might be contributing to it? What does it mean biologically?
+#For any assumption in the Wright-Fisher model, how might changing that assumption affect the result? How might nature and biology violate these assumptions?
+
+
+#ANSWER 1 EXPLAINING THE RESULTS OF FIG3 (exercise 3 part 1)
+#For my plot of population size vs time to fixation (fig3), what I expected to see was that larger populations should take more time to fix an allele than 
+#smaller populations. My prediction is not always true when I run the Wright-Fisher Model in exercise 3, 
+#likely due to the fact that the population sizes I chose are not that different from each other, so their resepective times to fixation may not be that different. 
+#If I chose a much higher population size, the behavior of the model would likely be more predictable, and time to fixation for an allele would 
+#essentially always take longer than for the smaller populations, like the ones I used. This is because larger populations experience less overall allele variation than 
+#smaller populations. 
+
+
+
+
+#ANSWER 2 ON THE ASSUMPTIONS OF THE MODEL- FIXED POPULATION
+#If the assumption that there is constant population size between generations was not true within the model, then that would mean that the fluctuation of alleles 
+#between each generation would not actually be random. Rather, allele fluctuation would likely be based on population size in each generation, which was demonstrated in 
+# exercise 3 part 1 where changing population sizes had an effect on time to fixation. Nature and biology violates this assumption because every generation
+#does not have a constant population size. 
+
+
+
+#ANSWER 3 ON THE ASSUMPTIONS OF THE MODEL- NO SELECTION
+#If the assumption that there is no natural selection was not true within the model, then alleles would not be able to be seen as evolutionarily neutral.
+#This is because some allele variations are deadly or significantly worse for survival than other allele variations that are passed through generations.
+#Thus, you would not be able to broadly understand an allele's fixation because of the high levels of outside pressure from selection agents, especially because
+#selection is very dynamic and individualized depending on the population you are looking at. Natural selection also affects other variables that are fixed
+# in the Wright-Fisher Model, like population, which further convolutes the fixation of an allele with selection present. 
+#In nature/biology, not every allele is created equal in terms of the outcome for the organism, but the Wright-Fisher Model assumes that this is true. 
 
 
