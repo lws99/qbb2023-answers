b8d70a0aff0272d019654be883cb430e906daa7c
Logan Wallace Shepard <lshepar8@jh.edu>
2023-12-14 18:59:31 -0500

    first step

---
+++ week-8/week8_assignment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+
+#Rscript runChicago.R GM_rep1.chinput,GM_rep2.chinput,GM_rep3.chinput GM12878 --design-dir . --en-feat-list featuresGM.txt --export-format washU_text
+
+
+#
+
+
+
+
+
+
+
